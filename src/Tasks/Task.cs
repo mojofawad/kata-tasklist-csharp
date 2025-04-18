052453ecfdaf381a9ff9da5740ce7eba33bfcf28
mojofawad <mojofawad@gmail.com>
2025-04-18 13:38:09 -0400

    . r: reformat Task.cs code

--- src/Tasks/Task.cs
+++ src/Tasks/Task.cs
@@ -3,23 +3,23 @@ using System.Collections.Generic;
 
 namespace Tasks
 {
-	public class Task
-	{
-		public long Id { get; set; }
+    public class Task
+    {
+        public long Id { get; set; }
 
-		public string Description { get; set; }
+        public string Description { get; set; }
 
-		private bool done;
-		public bool Done => done;
+        private bool done;
+        public bool Done => done;
 
-		public void MarkDone()
-		{
-			done = true;
-		}
+        public void MarkDone()
+        {
+            done = true;
+        }
 
-		public void MarkNotDone()
-		{
-			done = false;
-		}
-	}
+        public void MarkNotDone()
+        {
+            done = false;
+        }
+    }
 }
